c67dd021384f5fa21f68b1caa458d5f6593b7f6a
Giandre Acosta <soccre194@hotmail.com>
2017-01-28 14:52:13 -0500

    SQL db table shows if user enters correct username and pass

--- WebAdmin/sql.php
+++ WebAdmin/sql.php
@@ -7,7 +7,6 @@
     <li><a href="home.html">Home</a></li>
     <li><a href="cp.html">Control Panel</a></li>
     <li><a href="data.html">Data</a></li>
-    <li><a class="active" href="sql.php"></a></li>
     <li><a href="help.html">Help</a></li>
     <li><a href="login.html">Login</a></li>
 </ul>
@@ -15,32 +14,51 @@
 
 <?php
 
-$servername = "localhost";
-$username = "user";
-$password = "pass";
-$dbname = "CATS";
+$user = $_GET['admin'];
+$pass = $_GET['pass'];
+$myuser = stripslashes($user);
+$mypass = stripslashes($pass);
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+    if(isset($myuser, $mypass)) {
+        ob_start();
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully" . "<br>";
+        $servername = "localhost";
+        $username = "user";
+        $password = "pass";
+        $dbname = "CATS";
 
-$sql = "SELECT * FROM USER";
-$result = $conn->query($sql);
+    // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-if ($result->num_rows > 0) {
-// output data of each row
-    echo "<br>" . "First Name" . "\t" . "Last Name" . "\t" . "CUID" . "<br>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<br>" . $row["firstName"] . "\t" . $row["lastName"] . "\t" .$row["CUID"];
+    // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+//        echo "Connected successfully" . "<br>";
+
+
+        $sql = "SELECT * FROM ADMIN where Admin= '$myuser' and Password= '$mypass'";
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows == 1) {
+            echo "correct username and password";
+
+            $sql = "SELECT * FROM USER";
+            $result = $conn->query($sql);
+
+            // output data of each row
+            echo "<br>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<br>" . $row["firstName"] . " " . $row["lastName"] . " " . $row["CUID"];
+            }
+        } else
+            echo "invalid password and username";
+
+        ob_end_flush();
+
+        mysqli_close($conn);
     }
-}
-
-mysqli_close($conn);
 
 ?>
 
